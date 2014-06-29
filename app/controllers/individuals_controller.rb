c272eb8d5a416c3f89220e3bbeeb2b3efd995d21
Ellsworth Campbell <ells@psu.edu>
2014-06-28 20:37:50 -0600

    individual form creation controller and views

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,7 +1,21 @@
 class IndividualsController < ApplicationController
-  before_filter :initialize_individual
+  before_filter :create
 
-  def initialize_individual
-    @individual = Individual.new
+  def create
+    @individual = Individual.new(individual_params)
+    if @individual.save
+      # render '/static_pages/upload'
+    end
   end
+
+  def show
+    @user = Individual.find(params[:id])
+
+  end
+
+  private
+  def individual_params
+    params.require(:individual).permit(:heightFeet, :heightInches, :weight, :male, :chronic, :current)
+  end
+
 end
